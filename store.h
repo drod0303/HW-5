0bfa0fb075943109761813297746610daf3b46b0
drod0303 <drod0303@yahoo.com>
2016-10-05 23:59:58 -0500

    User interfaces through command line
    started additions to command line that allow the user to interact with
    the program

--- store.h
+++ store.h
@@ -8,21 +8,50 @@
 //vector<Media> publication_list;
 //Media media;
 
-void print_directions()
+void print_directions(int path)
 {
+  if(path == 0)
+  {
+    cout << endl;
+    cout << "1  (C)reate" << endl;
+    cout << "2  (R)eport" << endl;
+    cout << "3  (S)ave" << endl;
+    cout << "4  (Q)uit" << endl;
+    cout << endl;
+  }
+  else if(path ==1)
+  {
+    cout << endl;
+    cout<< "Create" << endl;
+    cout << "--------" << endl;
+    cout << "1.1  (O)rder" << endl;
+    cout << "1.2  (C)ustomer" << endl;
+    cout << "1.3  (S)ales Associate" << endl;
+    cout << "1.4  Robot (M)odel" << endl;
+    cout << "1.5  Robot (C)omponent" << endl;
+    cout << "1.6  (Q)uit to Main Menu" << endl;
+    cout << endl;
+  }
+  else if(path == 2)
+  {
+    cout << endl;
+    cout<< "Report" << endl;
+    cout << "--------" << endl;
+    cout << "2.1  (O)rders" << endl;
+    cout << "2.2  (C)ustomers" << endl;
+    cout << "2.3  (S)ales Associates" << endl;
+    cout << "2.4  Robot (M)odels" << endl;
+    cout << "2.5  Robot (P)arts" << endl;
+    cout << "2.6  (Q)uit to Main Menu" << endl;
+    cout << endl;
+  }
 
-  cout << endl;
-  cout << "1  (C)reate" << endl;
-  cout << "2  (R)eport" << endl;
-  cout << "3  (S)ave" << endl;
-  cout << "4  (Q)uit" << endl;
-  cout << endl;
 }
 
 void test_input(int input)
 {
 
-  if ((input==1) || (input==2) || (input==3) || (input==4) || (input=='C')|| (input=='R') || (input=='S') || (input=='Q')){} //makes sure that the input is a valid key number
+  if ((input==1) || (input==2) || (input==3) || (input==4)){} //makes sure that the input is a valid key number
 
   else
   {
@@ -34,7 +63,7 @@ void test_input(int input)
 
 void execute(int input)
 {
-
+    print_directions(input);
 }
 
 
